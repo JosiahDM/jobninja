04035018575b08d2a46a513918ddee7442ab467c
Josiah Moye <jmoye@neo.rr.com>
2016-10-26 13:56:29 -0600

    Entities tested

--- JobJPA/src/entities/Company.java
+++ JobJPA/src/entities/Company.java
@@ -1,11 +1,14 @@
 package entities;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -21,7 +24,46 @@ public class Company {
 
 	@ManyToOne
 	@JoinColumn(name="companyuserid")
-	@JsonBackReference(value="user")
+//	@JsonBackReference(value="user")
 	private User user;
+	
+	@OneToMany(mappedBy="company")
+	private Set<Word> words;
+
+	public String getCompanyname() {
+		return companyname;
+	}
+
+	public void setCompanyname(String companyname) {
+		this.companyname = companyname;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Set<Word> getWords() {
+		return words;
+	}
+
+	public void setWords(Set<Word> words) {
+		this.words = words;
+	}
+
+	public int getCompanyid() {
+		return companyid;
+	}
+
+	@Override
+	public String toString() {
+		return "Company [companyid=" + companyid + ", companyname=" + companyname + ", user=" + user + ", words="
+				+ words + "]";
+	}
+	
+	
 	
 }
