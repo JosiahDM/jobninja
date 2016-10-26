c917031320b9531138c1b5a308847ead2ebb5bf1
Josiah Moye <jmoye@neo.rr.com>
2016-10-26 11:37:11 -0600

    User entity added

--- JobJPA/src/entities/User.java
+++ JobJPA/src/entities/User.java
@@ -14,6 +14,32 @@ public class User {
 	@Column(name="userid")
 	private int id;
 	
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + "]";
+	}
+	
+	
 	
 	
 }
