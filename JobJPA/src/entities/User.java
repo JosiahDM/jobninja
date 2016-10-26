8ab764d529027b0a430070686ee9c889a27e7857
Josiah Moye <jmoye@neo.rr.com>
2016-10-26 11:33:51 -0600

    User basics added

--- JobJPA/src/entities/User.java
+++ JobJPA/src/entities/User.java
@@ -1,5 +1,19 @@
 package entities;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="userid")
+	private int id;
+	
+	
+	
 }
