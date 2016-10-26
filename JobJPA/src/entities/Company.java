eb218d0d1e5ea925eb20a0fdeb6275a9f2ae41ae
stevenmadams <stevenadams@me.com>
2016-10-26 11:41:09 -0600

    More work to company entity

--- JobJPA/src/entities/Company.java
+++ JobJPA/src/entities/Company.java
@@ -1,15 +1,26 @@
 package entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Company {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private int idcompany;
 	
 	private String companyname;
-	
-	private int companyuserid;
 
+	@ManyToOne
+	@JoinColumn(name="companyuserid")
+	@JsonBackReference(value="user")
+	private User user;
 	
 }
