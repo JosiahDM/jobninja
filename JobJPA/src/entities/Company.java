40eebc3ea907d34df4366056c1c85048702921ef
stevenmadams <stevenadams@me.com>
2016-10-26 14:12:10 -0600

    :-)

--- JobJPA/src/entities/Company.java
+++ JobJPA/src/entities/Company.java
@@ -1,11 +1,14 @@
 package entities;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -23,5 +26,8 @@ public class Company {
 	@JoinColumn(name="companyuserid")
 	@JsonBackReference(value="user")
 	private User user;
+	
+	@OneToMany
+	private Set<Word> words;
 	
 }
