d173095db96a0f20db2e336e64b05ccac65ee5d4
Josiah Moye <jmoye@neo.rr.com>
2016-10-26 11:56:49 -0600

    Imports

--- JobJPA/src/entities/Company.java
+++ JobJPA/src/entities/Company.java
@@ -4,13 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-<<<<<<< HEAD
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-=======
->>>>>>> c917031320b9531138c1b5a308847ead2ebb5bf1
+
 
 @Entity
 public class Company {
