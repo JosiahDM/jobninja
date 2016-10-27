6f9e3d038004b8aa4e43bdc82f15893116043bba
Josiah Moye <jmoye@neo.rr.com>
2016-10-27 11:36:01 -0600

    SQL update and testController authentication

--- JobNinja/WebContent/app_client/controllers/testController.js
+++ JobNinja/WebContent/app_client/controllers/testController.js
@@ -1,14 +1,13 @@
 var app = angular.module('ninja');
 
-app.controller('testController', function($scope, $window) {
+app.controller('testController', function($scope, $window, authenticationService) {
 
+    // Returns undefined if no valid user logged in
+    var user = authenticationService.currentUser();
 
+    // If user exists, load it into this scope
+    if (user) {
+        $scope.user = user;
+    }
 
-    /* Temporarily disabled
-    $window.Traitify.setPublicKey("v7ippc8rj0hu7tev7pi8tr2iid");
-    $window.Traitify.setHost("https://api-sandbox.traitify.com");
-    $window.Traitify.setVersion("v1");
-    var assessmentId = 'cca2e69d-0068-426f-b9be-94f34f7fa893'; // Will have to change based on which user is logged in.
-    $window.Traitify.ui.load(assessmentId, ".assessment");
-    */
 });
--- JobNinja/WebContent/app_client/directives/testButton.js
+++ JobNinja/WebContent/app_client/directives/testButton.js
@@ -8,13 +8,14 @@ var app = angular.module('ninja');
 app.directive('takeTest', function($compile, $window) { // Going to need authenticationServivce here
     return {
         restrict: 'E',
+        scope : {
+            user : '='
+        },
         link : function($scope, $element, $attr) {
             var compiledBtn = null;
-            var user = {};
-            user.tookTest = false;
-
             // Function to load the Traitify test view or results
             $scope.loadTest = function() {
+
 /* Temporariliy commented out to not overdo the API calls for now. Uncomment to test
                 $window.Traitify.setPublicKey("v7ippc8rj0hu7tev7pi8tr2iid");
                 $window.Traitify.setHost("https://api-sandbox.traitify.com");
@@ -29,7 +30,7 @@ app.directive('takeTest', function($compile, $window) { // Going to need authent
 
             // If user hasn't taken the test, display a button for them to start
             // Otherwise, just load the test to display results.
-            if (!user.tookTest) {
+            if (!$scope.user.tookTest) {
                 var $button = `<button ng-click="loadTest()">Take Test</button>`;
                 if (compiledBtn === null) {
                     compiledBtn = $compile($button)($scope);
