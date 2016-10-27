98af98cc3144a6bec5166bfdfec51ad3c48c0c0f
Nick <saltblade@gmail.com>
2016-10-27 10:23:30 -0600

    check to see if username already exists at registration. updated queries to protect against sql injection

--- JobNinja/WebContent/app_client/controllers/registrationController.js
+++ JobNinja/WebContent/app_client/controllers/registrationController.js
@@ -1,16 +1,15 @@
 var app = angular.module('ninja');
 
 app.controller('registrationController', function($scope, registrationService, $location) {
-	console.log("herxdfhe");
+
 	$scope.submit = function(username, password){
-		console.log("there");
 	    registrationService.createUser(username, password)
 	    .then(function(response){
 	    	if (response.status === 200) {
 	    		$location.url('/');
 	    	}
 	    });
-	    
 	  }
+	
     console.log("Register");
 });
