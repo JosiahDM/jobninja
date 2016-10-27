3b78659791bcb3f3a529645e5fbece93c515c70d
Josiah Moye <jmoye@neo.rr.com>
2016-10-27 11:04:52 -0600

    Test taking/viewing area set up. Need to integrate authentication

--- JobNinja/WebContent/app_client/controllers/testController.js
+++ JobNinja/WebContent/app_client/controllers/testController.js
@@ -1,5 +1,14 @@
 var app = angular.module('ninja');
 
-app.controller('testController', function($scope) {
-    console.log("Test");
+app.controller('testController', function($scope, $window) {
+
+
+
+    /* Temporarily disabled
+    $window.Traitify.setPublicKey("v7ippc8rj0hu7tev7pi8tr2iid");
+    $window.Traitify.setHost("https://api-sandbox.traitify.com");
+    $window.Traitify.setVersion("v1");
+    var assessmentId = 'cca2e69d-0068-426f-b9be-94f34f7fa893'; // Will have to change based on which user is logged in.
+    $window.Traitify.ui.load(assessmentId, ".assessment");
+    */
 });
