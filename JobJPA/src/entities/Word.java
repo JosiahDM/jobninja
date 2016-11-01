9f522feac4ea6d9c78fd98dc71467a2ed5569fa2
Josiah Moye <jmoye@neo.rr.com>
2016-11-01 12:55:37 -0600

    Updated word equals and hashcode so the sets will have unique word values

--- JobJPA/src/entities/Word.java
+++ JobJPA/src/entities/Word.java
@@ -16,8 +16,6 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 @Table(name="words")
 public class Word {
 
-	
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="wordid")
@@ -74,7 +72,25 @@ public class Word {
 	public String toString() {
 		return "Word [id=" + id + ", value=" + value + ", user=" + user + ", company=" + company + "]";
 	}
-	
+
+	@Override
+	public int hashCode() {
+		int hash = 7;
+//		hash = 31 * hash + id;
+		hash = 31 * hash + (null == value ? 0 : value.hashCode());
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		if((obj == null) || (obj.getClass() != this.getClass()))
+			return false;
+		// object must be Test at this point
+		Word word = (Word)obj;
+		return (value != null && value.equals(word.getValue()));
+	}
 	
 	
 	
